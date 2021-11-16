d06b5b8c760ff80c43d4ed6e2bc2ed0e941b7024
bartvanbenthem <bartvanbenthem@hotmail.com>
2021-11-16 13:26:02 +0100

    update controller

--- controllers/agent_controller.go
+++ controllers/agent_controller.go
@@ -281,6 +281,6 @@ func getPodNames(pods []corev1.Pod) []string {
 func (r *AgentReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&azdevopsv1alpha1.Agent{}).
-		Owns(&corev1.Secret{}).
+		Owns(&corev1.ConfigMap{}).
 		Complete(r)
 }
